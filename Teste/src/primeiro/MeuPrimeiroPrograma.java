deefad3bff63c69ff00a6c93922c00c820ae99f4
Pichau <Pichau@DESKTOP-1C9DI9E>
2021-07-14 12:00:30 -0300

    Meu primeiro commit!

---
+++ Teste/src/primeiro/MeuPrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package primeiro;
+
+public class MeuPrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Meu primeiro programa em java!");
+
+	}
+
+}
